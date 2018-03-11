0c779bff5ceea2a9509dcf42d95576c7a2d96f66
saikrishnaa97 <saikrishnaa.97@gmail.com>
2018-03-11 09:58:47 +0530

    C2 : Home activity is being worked on.

--- app/src/main/java/example/com/mydiary/view/HomeActivity.kt
+++ app/src/main/java/example/com/mydiary/view/HomeActivity.kt
@@ -2,12 +2,39 @@ package example.com.mydiary.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.Toast
 import example.com.mydiary.R
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(){
+
+    private var mToolbar : Toolbar?=null
+    private var mRecyclerView : RecyclerView ?= null
+    private var mAddButton : ImageButton ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        mToolbar = findViewById(R.id.home_toolbar)
+        mToolbar?.title = ""
+        setSupportActionBar(mToolbar)
+        mRecyclerView = findViewById(R.id.data_list_home)
+        mAddButton = findViewById(R.id.add_entry)
+        mAddButton?.setOnClickListener {
+            Toast.makeText(this,"Add button clicked",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
